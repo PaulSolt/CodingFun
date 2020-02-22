783c2daa88dcc9c98a42844479f884df9bac0295
Paul Solt <PaulSolt@gmail.com>
2020-02-22 18:48:57 -0500

    Initial Commit

---
+++ CodingFun/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CodingFun
+//
+//  Created by Paul Solt on 2/22/20.
+//  Copyright © 2020 Paul Solt. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
